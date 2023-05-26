df26822743fee6c9baacd4eeb3af47202919cdf5
Leonardo Moura <123783174+Vita-Lumina@users.noreply.github.com>
2023-05-26 17:46:00 -0300

    Update Script.js

--- Script/Script.js
+++ Script/Script.js
@@ -23,7 +23,35 @@ navLinks.forEach((link) => {
   });
 });
 
+// Obtém o elemento do botão de alternância
+var toggleButton = document.getElementById('toggleMode');
+// Obtém o elemento do ícone
+var icon = document.getElementById('toggleMode');
 
+// Adiciona um ouvinte de eventos para o clique no botão
+toggleButton.addEventListener('click', function() {
+    // Obtém o elemento de folha de estilo
+    var styleSheet = document.getElementById('styleSheet');
+
+    // Verifica qual folha de estilo está sendo usada atualmente
+    if (styleSheet.getAttribute('href') === 'Style/estilo.css') {
+        // Alterna para o estilo dark
+        styleSheet.setAttribute('href', 'Style/estilodark.css');
+        // Alterna para o ícone da lua
+        icon.setAttribute('src', 'IMG/Moon.ico');
+        icon.setAttribute('alt', 'Moon Icon');
+    } else {
+         // Alterna para o estilo padrão
+         styleSheet.setAttribute('href', 'Style/estilo.css');
+         // Alterna para o ícone do sol
+         icon.setAttribute('src', 'IMG/Sun.ico');
+         icon.setAttribute('alt', 'Sun Icon');
+    }
+});
+
+
+
+//Slides de experiencia
 $(document).ready(function(){
   $('.exp-slider').slick({
     dots: true,
@@ -63,5 +91,37 @@ $(document).ready(function(){
         }
       }
     ]
+  });
+
+//Slide de certificado
+
+$(document).ready(function(){
+  $('.cert-slider').slick({
+    dots: true,
+    centerMode: false,
+    centerPadding: '0px',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    adaptiveWidth: true
+  });
+
+  $('.cert-slider').on('init', function(event, slick){
+    $('.cert-item.slick-current').addClass('active'); // Adiciona a classe 'active' ao primeiro slide
+  });
+
+  $('.cert-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $('.cert-item').removeClass('active'); // Remove a classe 'active' de todos os slides
+  });
+
+  $('.cert-slider').on('afterChange', function(event, slick, currentSlide){
+    $('.cert-item[data-slick-index="' + currentSlide + '"]').addClass('active'); // Adiciona a classe 'active' ao slide atual
+  });
+});
+
+$(document).on('click', '.cert-item', function(e){
+  e.preventDefault();
+  var slideIndex = $(this).data('slick-index');
+  $('.cert-slider').slick('slickGoTo', slideIndex); // Navega para o slide correspondente ao clicar em um item
   });
 });
